02e62a6828c72a9e21715fec4986117fdce0f095
kaala <kaala@users.noreply.github.com>
2025-10-13 22:09:06 +0800

    Update and rename wificheck.sh to 20-wificheck.sh

---
+++ scripts/20-wificheck.sh
@@ -0,0 +1,20 @@
+cat << "eof" > /tmp/wificheck.sh
+
+sleep 30
+
+while :
+do
+
+wifi isup
+
+r=$?
+test $r = 0 && echo 0 > /sys/class/leds/blue:system/brightness
+test $r = 0 || echo 1 > /sys/class/leds/blue:system/brightness
+test $r = 0 || wifi
+
+sleep 600
+done
+
+eof
+
+sh /tmp/wificheck.sh &
